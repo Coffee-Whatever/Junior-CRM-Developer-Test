e54ee92df1f18c0e38042d3a2fabfb76a9231c4d
Coffee-Whatever (Patryk) <patryk.k.priv@outlook.com>
2024-06-13 22:07:06 +0200

    First day progress. Database is created, but not filled with data. SignIn page done Work began on Employee view. TODO: *Add data to DB. *Finnish Employee view. *Create PM, HRM and administrator views.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,24 +1,91 @@
-﻿using System.Text;
+﻿using MySql.Data.MySqlClient;
+using System.Data;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Security.Cryptography;
+using System.Windows.Documents.Serialization;
+using System.ComponentModel;
 
 namespace Junior_CRM_Developer_Test
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        private void SignInParsing(object sender, RoutedEventArgs e)
+        {
+            string log = login.Text;
+            string salt = "";
+            try
+            {
+                string connetionstring = "Server=localhost;Database=crmtest;Uid=root;";
+                MySqlConnection conn = new MySqlConnection(connetionstring);
+                conn.Open();
+
+                string query = $"SELECT current_salt FROM `credentials` WHERE login = \"{log}\";";
+
+                MySqlDataAdapter dtb = new MySqlDataAdapter();
+                dtb.SelectCommand = new MySqlCommand(query, conn);
+                DataTable dtable = new DataTable();
+                dtb.Fill(dtable);
+                salt = dtable.Rows[0].ItemArray[0].ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Incorrect login or password");
+            }
+
+            string pass = GetHashString(password.Password, salt);
+            try
+            {
+                string connetionstring = "Server=localhost;Database=crmtest;Uid=root;";
+                MySqlConnection conn = new MySqlConnection(connetionstring);
+                conn.Open();
+
+                string query = $"SELECT CASE `password` WHEN '{pass}' THEN 1 ELSE 0 END isCorrect FROM `credentials` WHERE login = \"{log}\";";
+
+                MySqlDataAdapter dtb = new MySqlDataAdapter();
+                dtb.SelectCommand = new MySqlCommand(query, conn);
+                DataTable dtable = new DataTable();
+                dtb.Fill(dtable);
+                if (dtable.Rows[0].ItemArray[0].ToString() == "0")
+                {
+                    MessageBox.Show("Incorrect login or password");
+                }
+                else
+                {
+                    Menu menu = new Menu(log);
+                    menu.ShowActivated = true;
+                    menu.Closing += (object sender, CancelEventArgs e) =>
+                    {
+                        this.Show();
+                    };
+                    menu.Show();
+
+                    this.Hide();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Incorrect login or password");
+            }
+        }
+        private static byte[] GetHash(string inputString)
+        {
+            using (HashAlgorithm algorithm = SHA256.Create())
+                return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+        }
+        private static string GetHashString(string inputString, string salt)
+        {
+            //string salt = RandomNumberGenerator.GetString("1234567890!@#$%^&*()qwertyuiopasdfghjklzxcvbnm,.<>:;[]{}", 20);
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in GetHash(String.Concat(inputString, salt)))
+                sb.Append(b.ToString("X2"));
+
+            return sb.ToString();
         }
     }
 }
---
+++ Menu.xaml.cs
@@ -0,0 +1,119 @@
+﻿using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI.Relational;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Xml.Linq;
+using static Mysqlx.Expect.Open.Types.Condition.Types;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Junior_CRM_Developer_Test
+{
+    /// <summary>
+    /// Interaction logic for Menu.xaml
+    /// </summary>
+    public partial class Menu : Window
+    {
+        string login;
+        public ObservableCollection<Project> Projects { get; set; } = new();
+        public Menu(string login)
+        {
+            this.login = login;
+            InitializeComponent();
+        }
+        private void PassedSignIn()
+        {
+            try
+            {
+                string connetionstring = "Server=localhost;Database=crmtest;Uid=root;";
+                MySqlConnection conn = new MySqlConnection(connetionstring);
+                conn.Open();
+
+                string query = $"SELECT access_level FROM `credentials` WHERE login = \"{this.login}\";";
+
+                MySqlDataAdapter dtb = new MySqlDataAdapter();
+                dtb.SelectCommand = new MySqlCommand(query, conn);
+                DataTable dtable = new DataTable();
+                dtb.Fill(dtable);
+
+                string access = dtable.Rows[0].ItemArray[0].ToString();
+                if (access == "base")
+                {
+                    BaseUser();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Incorrect login or password");
+            }
+        }
+        public void BaseUser()
+        {
+            {
+                string connetionstring = "Server=localhost;Database=crmtest;Uid=root;";
+                MySqlConnection conn = new MySqlConnection(connetionstring);
+                conn.Open();
+
+                string query = $"SELECT * FROM projects" +
+                    $"JOIN employeeprojectrelations ON projects.id = employeeprojectrelations.projectId" +
+                    $"JOIN employees ON employeeprojectrelations.employeeId = employees.id" +
+                    $"WHERE employees.id = {0};";
+
+                MySqlDataAdapter dtb = new MySqlDataAdapter();
+                dtb.SelectCommand = new MySqlCommand(query, conn);
+                DataTable dtable = new DataTable();
+                dtb.Fill(dtable);
+                foreach (DataRow row in dtable.Rows)
+                {
+                    Project project = new Project();
+                    project.Type = row.ItemArray[1].ToString();
+                    project.StartDate = Convert.ToDateTime(row.ItemArray[2]);
+                    project.EndDate = Convert.ToDateTime(row.ItemArray[3]);
+                    project.Manager = row.ItemArray[4].ToString();
+                    project.Status = row.ItemArray[6].ToString();
+                    project.Comment = row.ItemArray[5].ToString();
+                }
+            }
+            {
+                Border border = new Border();
+                border.Margin = new Thickness(10);
+                border.BorderThickness = new Thickness(2);
+                border.BorderBrush = Application.Current.TryFindResource("AccentBrush") as Brush;
+
+                DataGrid dataGrid = new DataGrid();
+
+                Binding myBinding = new Binding("ItemsSource");
+                myBinding.Source = Projects;
+
+                dataGrid.SetBinding(DataGrid.ItemsSourceProperty, myBinding);
+
+                border.Child = dataGrid;
+                Main.Children.Add(border);
+            }
+            {
+
+            }
+        }
+        public class Project
+        {
+            public string Type { get; set; }
+            public DateTime StartDate { get; set; }
+            public DateTime EndDate { get; set; }
+            public string Manager { get; set; }
+            public string Status { get; set; }
+            public string Comment { get; set; }
+        }
+    }
+}
